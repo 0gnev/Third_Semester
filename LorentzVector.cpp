c4ffb6ca3d89f487a117800e7435d0af7687ce12
0gnev <49639884+0gnev@users.noreply.github.com>
2019-09-27 13:42:02 +0700

    Main files

---
+++ LorentzVector.cpp
@@ -0,0 +1,6 @@
+#include "LorentzVector.h"
+using namespace std;
+
+LorentzVector::LorentzVector(){
+    x=0; y=0; z=0; t=0;
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "LorentzVector.h"
+using namespace std;
+
+int main(){
+    LorentzVector vector1;
+    return 0;
+}   
